4288b3b00ac525bdb15958796a76286c738b7905
Mikołaj Sikora <mikolajsikora98@gmail.com>
2021-02-24 16:22:34 +0100

    HS-161 rewrite basis of model to scala

---
+++ src/main/scala/pl/agh/harmonytools/model/util/ChordComponentManager.scala
@@ -0,0 +1,34 @@
+package pl.agh.harmonytools.model.util
+
+import pl.agh.harmonytools.model.ChordComponent
+import scala.collection.immutable.HashMap
+
+object ChordComponentManager {
+
+  private var availableChordComponents: HashMap[(String, Boolean), ChordComponent] = HashMap.empty
+
+  def getRoot: ChordComponent = chordComponentFromString("1")
+
+  def chordComponentFromString(chordComponentString: String, isDown: Boolean = false): ChordComponent = {
+    availableChordComponents.find(_._1 == (chordComponentString, isDown)) match {
+      case Some((key, value)) => value
+      case None =>
+        val cc = ChordComponent(chordComponentString, isDown)
+        availableChordComponents = availableChordComponents.updated((chordComponentString, isDown), cc)
+        cc
+    }
+  }
+
+  @deprecated
+  def basicChordComponentFromPitch(pitch: Int, isDown: Boolean): ChordComponent = {
+    pitch match {
+      case 3 => chordComponentFromString("3>", isDown)
+      case 4 => chordComponentFromString("3", isDown)
+      case 5 => chordComponentFromString("3<", isDown)
+      case 6 => chordComponentFromString("5>", isDown)
+      case 7 => chordComponentFromString("5", isDown)
+      case 8 => chordComponentFromString("5<", isDown)
+    }
+  }
+
+}
